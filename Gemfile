f97fa54b15ac8e2a94f5c186a52c82faa8622b06
A1exsandro <alexsandro.acp@gmail.com>
2022-08-25 19:46:33 -0400

    basic config simple_token

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '3.0.2'
 gem 'rack-cors'
 gem 'rack-attack'
 gem 'devise'
+gem 'simple_token_authentication'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.6', '>= 6.1.6.1'
--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,11 @@
 module Api::V1
-  class ApiController < ApplicationController 
-    #Métodos globais
+  class ApiController < ApplicationController
+    acts_as_token_authentication_handle_for User
+    before_action :require_authetication!
+    
+    private
+      def require_authetication!
+        throw(:warden, scope: :user) unless current_user.presence
+      end
   end
 end
